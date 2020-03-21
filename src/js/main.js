372c6d8bc7689ff06acf04287c1a7727aa93b980
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-03-21 21:40:11 +0300

    Header: pagination

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,14 @@ $(function(){
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/pagination/arrow-left.svg" alt=""></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/pagination/arrow-right.svg" alt=""></img>'
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/pagination/arrow-right.svg" alt=""></img>',
+        asNavFor: '.slider-pagination'
     });
 
-  
+    $('.slider-pagination').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
+
 });

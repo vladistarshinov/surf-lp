f7eb0fcf08c14eba8c6f61b5cddc68f646ca8f01
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-04-12 11:42:31 +0300

    JS: anchor. CSS: remove horizontal scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -194,3 +194,13 @@ $(function(){
 
 
 });
+
+
+$(document).ready(function(){
+  /* Плавный переход между секциями */
+  $('a[href^="#"]').click(function(){ 
+      let anchor = $(this).attr('href');  
+      $('html, body').animate({           
+      scrollTop:  $(anchor).offset().top}, 2000);                            
+  });
+  });

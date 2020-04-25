ba7c255fd7f1af36ba926e77d91bf77703ae9007
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-04-25 15:12:35 +0300

    Contacts buttons plugin: 2 modals, adaptive

--- src/js/anim.js
+++ src/js/anim.js
@@ -1,5 +1,15 @@
 $(function(){
 
-    new WOW().init();
+    var isMobile = false;
+    // check width's screen
+    $(document).ready( function() {
+        if ($('body').width() <= 480) {
+            isMobile = true;
+        }
+        // flag >md
+        if (!isMobile) {
+            new WOW().init();
+        }
+    } );
 
 });

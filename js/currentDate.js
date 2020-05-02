8f4ec8d75260544c77d28dbdfb4878e7fbcca0c3
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-05-02 19:51:21 +0300

    ES6, bundle

--- js/currentDate.js
+++
@@ -1,7 +0,0 @@
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
-
-document.getElementById('day').innerHTML = day;
-document.getElementById('m_y').innerHTML = month + ' | ' + year;
